8e8d487366ee7536552ba700cb6ccb40ac2aeb50
dat <21211tt0848@mail.tdc.edu.vn>
2023-05-17 21:39:50 +0700

    update role by dat

---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,64 @@
+@extends('admin.layouts.app')
+@section('title', 'Users')
+@section('content')
+    <div class="card">
+
+        <h1>
+            User list
+        </h1>
+        @if (session('message'))
+            <h1 class="text-primary">{{ session('message') }}</h1>
+        @endif
+        @can('create-user')
+            <div>
+                <a href="{{ route('users.create') }}" class="btn btn-primary">Create</a>
+
+            </div>
+        @endcan
+        <div>
+            <table class="table table-hover">
+                <tr>
+                    <th>#</th>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+
+                @foreach ($users as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td><img src="{{ $item->images->count() > 0 ? asset('upload/' . $item->images->first()->url) : 'upload/default.png' }}"
+                                width="200px" height="200px" alt=""></td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->email }}</td>
+
+                        <td>{{ $item->phone }}</td>
+                        <td>
+                            @can('update-user')
+                                <a href="{{ route('users.edit', $item->id) }}" class="btn btn-warning">Edit</a>
+                            @endcan
+                            @can('delete-user')
+                                <form action="{{ route('users.destroy', $item->id) }}" id="form-delete{{ $item->id }}"
+                                    method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <button class="btn btn-delete btn-danger" type="submit"
+                                        data-id={{ $item->id }}>Delete</button>
+
+                                </form>
+                            @endcan
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+            {{ $users->links() }}
+        </div>
+
+    </div>
+
+@endsection
+
+@section('script')
+@endsection
