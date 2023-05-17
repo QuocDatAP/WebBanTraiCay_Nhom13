cb37e5a7ee06e6ebbff86c4d365e0abd909a058c
Bao <baoharry101@gmail.com>
2023-05-17 21:53:32 +0700

    crud_coupon

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\CounponController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\RoleController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Client\HomeController;
+use App\Http\Controllers\Client\ProductController as ClientProductController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +17,42 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// route client
+Route::get('/', [HomeController::class, 'index'])->name('client.home');
+
+
+Route::get('product/{category_id}', [ClientProductController::class, 'index'])->name('client.products.index');
+Route::get('product-detail/{id}', [ClientProductController::class, 'show'])->name('client.products.show');
+
+
+
+
+Auth::routes();
+
+
+// route admin
+Route::middleware('auth')->group(function(){
+
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard.index');
+    })->name('dashboard');
+
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('coupons', CounponController::class);
+
 });
+
+
+
+
+
+
+
